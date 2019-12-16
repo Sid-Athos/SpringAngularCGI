9cfb6f70e433f2770a401b9a1f2c90a7d39a6873
SidBennaceur <sid-ahmed.bennaceur@cgi.com>
2019-12-16 23:17:21 +0100

    first table modified, on the way to create second one

---
+++ src/main/java/com/example/demo/pojo/BookJSON.java
@@ -0,0 +1,20 @@
+package com.example.demo.pojo;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
+
+@Data
+public class BookJSON implements Serializable {
+
+    // Serializable to avoid Cannot serialize redis ERROR !
+
+    private Long id;
+    @NotEmpty
+    private String title;
+    @NotEmpty
+    private String author;
+    @NotEmpty
+    private String troubles;
+}
---
+++ src/main/resources/db/runtime.sql
@@ -0,0 +1,14 @@
+INSERT INTO public.book(id, title, author,troubles) VALUES (0, 'LOTR 1', 'JR
+Tolkien',
+'Nein');
+INSERT INTO public.book(id, title, author,troubles) VALUES (1, 'LOTR 2', 'JR Tolkien',
+'nein');
+INSERT INTO public.book(id, title, author,troubles) VALUES (2, 'LOTR 3', 'JR Tolkien',
+'da');
+
+--INSERT INTO public.User(id, name, pwd,mail)
+--VALUES (0, 'LOTR 1', 'JRTolkien','Nein@da.com');
+--INSERT INTO public.User(id, name, pwd,mail)
+--VALUES (0, 'LOTR 1', 'JRTolkien','Nein@ya.com');
+--INSERT INTO public.User(id, name, pwd,mail)
+--sVALUES (0, 'LOTR 1', 'JRTolkien','Nein@fuck.com');
