0a934f647eaf177bcc11ba4a597827a36ae79086
SidBennaceur <sid-ahmed.bennaceur@cgi.com>
2019-12-20 20:08:08 +0100

    fixed insert

--- src/main/resources/db/runtime.sql
+++ src/main/resources/db/runtime.sql
@@ -1,9 +1,13 @@
 -- Queries that'll be run at launch
 
-INSERT INTO public.post(postdate, title, author,content) VALUES (CURRENT_TIMESTAMP(), 'Aliquam dictum dui vel', 'Sylvanas Windrunner', 'enean facilisis justo accumsan, et lorem elit ?');
-INSERT INTO public.post(postdate, title, author,content) VALUES (CURRENT_TIMESTAMP(), ' Sed pharetra', 'Arthas Menethil','Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc eget nulla sit amet ligula semper auctor. ');
-INSERT INTO public.post(postdate, title, author,content) VALUES (CURRENT_TIMESTAMP(), 'Duis a imperdiet libero', 'Jaina Proudmoore',  'Donec pellentesque, elit sit amet semper ornare, elit tortor molestie dui, commodo tincidunt turpis est id ligula. Fusce vitae ligula non lacus auctor finibus in id augue. Mauris lobortis libero sit amet nibh egestas, condimentum mattis orci iaculis. ');
-INSERT INTO public.post(postdate, title, author,content) VALUES (CURRENT_TIMESTAMP(), 'Lorem ipsum', 'Illidan Stormrage',
+INSERT INTO public.post(postdate, title, author,content) VALUES
+(CURRENT_TIMESTAMP(), 'Aliquam dictum dui vel', '1', 'enean facilisis justo accumsan, et lorem elit ?');
+INSERT INTO public.post(postdate, title, author,content) VALUES
+(CURRENT_TIMESTAMP(), ' Sed pharetra', '2','Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc eget nulla sit amet ligula semper auctor. ');
+INSERT INTO public.post(postdate, title, author,content) VALUES
+(CURRENT_TIMESTAMP(), 'Duis a imperdiet libero', '2',  'Donec pellentesque, elit sit amet semper ornare, elit tortor molestie dui, commodo tincidunt turpis est id ligula. Fusce vitae ligula non lacus auctor finibus in id augue. Mauris lobortis libero sit amet nibh egestas, condimentum mattis orci iaculis. ');
+INSERT INTO public.post(postdate, title, author,content) VALUES
+(CURRENT_TIMESTAMP(), 'Lorem ipsum', '3',
 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque et lorem elit. Aliquam dictum dui vel sapien ultrices aliquet. Aenean facilisis justo accumsan semper dapibus. Curabitur id ex mollis, dictum leo ultrices.');
 
 INSERT INTO public.user( name, pwd,mail)
@@ -22,14 +26,14 @@ INSERT INTO public.likes(post,author) VALUES ( '3','3');
 
 
 INSERT INTO public.comment(date, author,content,postid) VALUES
-(CURRENT_TIMESTAMP(),'Arthas Menethil','kek', 1);
+(CURRENT_TIMESTAMP(),'1','kek', '1');
 INSERT INTO public.comment(date, author,content,postid) VALUES
-(CURRENT_TIMESTAMP(),'Illidan Stormrage','git gud', 1);
+(CURRENT_TIMESTAMP(),'3','git gud', '1');
 
 INSERT INTO public.comment(date, author,content,postid) VALUES
-(CURRENT_TIMESTAMP(),'Sylvanas Windrunner',
-'Aliquam mattis vestibulum neque in porta ! :DD', 4);
+(CURRENT_TIMESTAMP(),'2',
+'Aliquam mattis vestibulum neque in porta ! :DD', '4');
 
 INSERT INTO public.comment(date, author,content,postid) VALUES
-(CURRENT_TIMESTAMP(),'Illidan Stormrage',
-'bien dit', 4);
+(CURRENT_TIMESTAMP(),'2',
+'bien dit', '4');
